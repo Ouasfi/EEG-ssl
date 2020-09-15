7d19abd5f78cc17c1ddcf8c0dbdbc64c934ac9ea
Ouasfi <amine1ouasfi@hotmail.f>
2020-09-15 20:09:15 +0200

    add general settings and paths

---
+++ settings.py
@@ -0,0 +1,15 @@
+
+subjects = ['P01','P02','P03','P04','P05','P06','P07','P08','P09','P10',
+            'P11','P12']
+
+recording_with_mastoid_channels =['Pilot3','P01','P02','P03','P04','P05','P06','P07','P08']
+CONDITIONS = ['cued', 'non-cued', 'free']
+KEYSTROKE_BASE_ID = 2000
+STIMULUS_IDS = [1, 2, 3, 4, 11, 12, 13, 14, 21, 22, 23, 24]
+v_1_subjects =  ['Pilot3','P01','P04','P05','P06','P07'] 
+RAW_DIR = "/kaggle/input/rawsbeforehoes/"
+ICA_DIR = '/kaggle/input/openmiir/eeg/preprocessing/ica/'
+METADATA_DIR = "/kaggle/input/openmiir"
+DATA_PATH = METADATA_DIR
+
+DEFAULT_VERSION = 2

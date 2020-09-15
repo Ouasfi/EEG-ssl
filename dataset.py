217724529baf05c3bd89576b637c057a00d4479a
Ouasfi <amine1ouasfi@hotmail.f>
2020-09-15 20:11:40 +0200

    add model and dataset class

---
+++ dataset.py
@@ -0,0 +1,110 @@
+from pylab import *
+import torch 
+
+class WeightedSampler(torch.utils.data.sampler.Sampler):
+    r"""Sample des windows randomly
+    Arguments:
+    ---------
+        dataset (Dataset): dataset to sample from
+        size (int): The total number of sequences to sample
+    """
+
+    def __init__(self,dataset, batch_size,size,  weights):
+        
+        
+        self.batch_size = batch_size
+        self.size = size
+        self.dataset = dataset
+        self.serie_len = len(self.dataset)
+        
+        self.weights = torch.DoubleTensor(weights)
+        
+    def __iter__(self):
+        num_batches = self.size// self.batch_size
+        while num_batches > 0:
+            print()
+            sampled = 0
+            while sampled < self.batch_size:
+                target  = 2*torch.multinomial(
+            self.weights, 1, replacement=True) -1
+                t = choice(arange(0, self.serie_len-dataset.temp_len, 1))
+                t_ = dataset.get_pos(t) if target>0 else dataset.get_neg(t)
+                sampled += 1
+                yield (t, t_, target)
+            
+            num_batches -=1
+
+    def __len__(self):
+        return len(self.train_list)   
+
+
+class Abstract_Dataset(torch.utils.data.Dataset):
+    '''
+    Classe dataset  pour les differents sampling
+    '''
+    def __init__(self, time_series, temp_len = T, n_features = C):
+
+        self.time_series = time_series
+        self.temp_len = temp_len
+        self.n_features = n_features
+    def get_windows(self,index):
+        '''
+        a method to load  a sequence 
+        '''
+        raise NotImplementedError
+    def get_pos(self, t_anchor):
+        '''
+        a method to get positive samples
+        '''
+        raise NotImplementedError
+    def get_neg(self, t_anchor):
+        '''
+       a method to get negative samples
+        '''
+        raise NotImplementedError
+    def get_targets(self, index):
+        '''
+        a method to get labels
+        '''
+        raise NotImplementedError
+    def __getitem__(self, index):
+        windows = self.get_windows(index)
+        target = self.get_targets(index)
+        return windows, target
+    def __len__(self): return self.time_series.shape[1]
+
+class RP_Dataset(Abstract_Dataset):
+    
+    def __init__(self, time_series, sampling_params, temp_len = T, n_features = C):
+      super().__init__(time_series, temp_len = temp_len, n_features = n_features)
+      self.pos , self.neg = sampling_params
+    def get_windows(self,index):
+        '''
+        a method to get sampled windows
+        '''
+        (t, t_ , _) = index
+        anchor_wind = self.time_series[:,t:t+self.temp_len]
+        neg_wind = self.time_series[:,t_:t_+self.temp_len] # could be negative or positive
+        return (anchor_wind, neg_wind)
+    
+    def get_targets(self, index):
+        return index[-1]
+    def get_pos(self, t_anchor):
+
+      start = min(0,t_anchor-self.pos ) 
+      end = min(self.__len__()-self.temp_len,t_anchor+self.pos ) # to get a sequence of lenght self.temp_lenght
+      t_ = choice(arange(start,end, 1)) 
+      return t_
+    def get_neg(self, t_anchor):
+      
+      left_idx = arange(0, min(0, t_anchor - self.neg), 1)
+      right_idx =arange(min(self.__len__()-self.temp_len, t_anchor + self.neg),self.__len__()-self.temp_len ,1)
+      t_ = choice(hstack([left_idx, right_idx]))
+      return t_
+
+def collate(batch):
+    anchors = torch.stack([item[0][0] for item in batch])
+    sampled = torch.stack([item[0][1] for item in batch])
+    targets = torch.stack([item[1] for item in batch])
+    
+    return (anchors, sampled), targets
---
+++ models.py
@@ -0,0 +1,76 @@
+import torch
+from torch.nn import *
+from pylab import *
+from torch import optim
+from torch.utils import data
+from torch import nn
+from torch.nn.functional import soft_margin_loss
+
+
+
+class StagerNet(Module):
+    """
+     StagerNet implementation.    
+    """
+    def __init__(self, num_classes, num_channels, temp_lenght ):
+        super().__init__()      
+        # create conversion layer
+        self.relu = ReLU()
+        self.spatial_conv = Conv2d(1, num_channels, (num_channels,1), stride= (1,1))
+        self.temp_conv1 =  Conv2d(1, 16, (1,51), stride= (1,1), padding= (0,(51-1)//2))#(51-1)//2 to insure same padding
+        self.batch_norm1 = BatchNorm2d(16)
+        self.temp_conv2 =  Conv2d(16, 16, (1,51), stride= (1,1), padding= (0,(51-1)//2))#(51-1)//2 to insure same padding
+        self.batch_norm2 = BatchNorm2d(16)
+        self.maxPool = MaxPool2d((1, 13), stride=(1, 13))
+        self.flatten = Flatten()
+        self.dropout = Dropout(p = 0.5)
+        self.linear_class = Linear(num_channels*(temp_lenght//(13*13))*16,num_classes )        
+    def forward(self, inputs):
+      x = self.spatial_conv(inputs)
+      x = x.permute(0,2,1,3)
+      x = self.relu(self.temp_conv1(x))
+      # a relu activation is used before batch_norm, is it the case in the original implementation ?
+      x = self.batch_norm1(x) 
+      x = self.relu(x)      
+      x = self.maxPool(x)
+      x = self.relu(self.temp_conv2(x)) 
+      x = self.batch_norm2 (x)
+      x = self.relu(x)
+      x = self.maxPool(x)
+      x = self.dropout(self.flatten(x))
+      x = self.linear_class(x)
+      return x
+
+
+      class ShallowNet(Module):
+    """
+     ShallowNet implementation.    
+    """
+    def __init__(self, num_classes, num_channels , temp_lenght):
+        super().__init__()      
+        # create conversion layer
+        self.eps = 1e-45
+        self.relu = ReLU()
+        
+        self.temp_conv1 =  Conv2d(1, 40, (1,25), stride= (1,1))
+        self.batch_norm2 = BatchNorm2d(40)
+        self.spatial_conv = Conv2d(40, 40, (num_channels,1), stride= (1,1))
+        self.meanPool = AvgPool2d((1, 75), stride=(1, 15))
+        self.flatten = Flatten()
+        self.dropout = Dropout(p = 0.5)
+        self.num_features = (((temp_lenght-25+1)-75)//15+1)*40
+        self.linear_class = Linear( self.num_features,num_classes )  
+
+    def forward(self, inputs):
+      x = self.temp_conv1(inputs)
+      x = self.batch_norm2(x)
+      x = self.spatial_conv(x)
+      x = torch.pow(x, 2) # squaring non-linearity
+
+      x = self.meanPool(x)
+      x = self.flatten(x)
+      x = torch.log(x+self.eps) #log  non-linearity 
+      
+      x = self.dropout(self.flatten(x))
+      x = self.linear_class(x)
+      return x

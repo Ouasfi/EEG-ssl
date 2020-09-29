3d04340fdee6057a01884cc5a7da10d91af64f8a
Ouasfi <amine1ouasfi@hotmail.fr>
2020-09-29 17:48:04 +0200

    add decoders

--- process.py
+++ process.py
@@ -10,15 +10,16 @@ def decode_event(event_id):
 def get_stim_events(events, stimulus_ids='all', conditions='all'):
     filtered = []
     for event in events:
-    event_id = event[2]
-    if event_id >= 1000:
-        continue
+        event_id = event[2]
+        if event_id >= 1000:
+            continue
 
-    stimulus_id, condition = decode_event(event_id)
+        stimulus_id, condition = decode_event(event_id)
 
-    if (stimulus_ids == 'all' or stimulus_id in stimulus_ids) and \
-        (conditions == 'all' or condition in conditions):
-    filtered.append(event)
+        if (stimulus_ids == 'all' or stimulus_id in stimulus_ids) and \
+            (conditions == 'all' or condition in conditions):
+            
+            filtered.append(event)
 
     return np.array(filtered)
 
